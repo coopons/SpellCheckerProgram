450ee5f1c01bec6c55d78b4888cf1a9b0a59fb5c
coopons <cooppons@gmail.com>
2023-04-15 19:33:47 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package spellCheck;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static String filePath = "src/Resources/Oxford_English_Dictionary.txt";
+	public static String word;
+	private static SpellCheckerTemp sc;
+
+	public static void main(String[] args) {
+		loadDictionary();
+		runSpellCheck();
+	}
+
+	private static void loadDictionary() {
+		sc.loadGraph(filePath);
+	}
+
+	private static void runSpellCheck() {
+		try (Scanner userInput = new Scanner(System.in)) {
+			System.out.println("Please enter a word: ");
+			word = userInput.next().toLowerCase().replaceAll("[^a-zA-Z]", "");
+			sc.isWordSpelledCorrectly(word);
+		}
+	}
+
+}
